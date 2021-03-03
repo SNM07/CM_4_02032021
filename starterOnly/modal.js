6b10aa2b9b5e125b1a04a921c20b8331b057fb43
SNM07 <say.no.more@hotmail.fr>
2021-03-03 09:40:01 +0100

    #1-[DONE]
    Close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,9 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+// Get the <span> element that closes the modal
+const modalClose = document.querySelectorAll(".close")[0];
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -20,4 +23,14 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// When the user clicks on <span> (x), close the modal
+modalClose.onclick = function() {
+  modalbg.style.display = "none";
+}
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalbg) {
+    modalbg.style.display = "none";
+  }
+}
