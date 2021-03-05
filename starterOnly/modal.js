e836a5b72a38dd1651fa5b33e04ec0135bcf6a4c
SNM07 <say.no.more@hotmail.fr>
2021-03-05 18:06:09 +0100

    #4 - WIP v3-b
    test

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -24,23 +24,51 @@ function launchModal() {
 }
 
 // When the user clicks on <span> (x), close the modal
-modalClose.onclick = function() {
+modalClose.onclick = function () {
   modalbg.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modalbg) {
     modalbg.style.display = "none";
   }
-}
+};
 
 function validate() {
-  e.preventDefault();
+  /* document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // generate a five digit number for the contact_number variable
+    this.contact_number.value = Math.random() * 100000 | 0;
+    // these IDs from the previous steps
+    emailjs.sendForm('contact_service', 'contact_form', this)
+        .then(function() {
+            console.log('SUCCESS!');
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
+  }); 
+  e.preventDefault(e);*/
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
   //setTimeout(validate(), 8000);
-  f(popup.classList.contains("show")) // Check if the popup is shown
-     setTimeout(() => popup.classList.remove("show"), 10000) // If yes hide it after 10000 milliseconds
-
+  f(popup.classList.contains("show")); // Check if the popup is shown
+  setTimeout(() => popup.classList.remove("show"), 10000); // If yes hide it after 10000 milliseconds
+  return false;
 }
+
+
+/*window.onload = function() {
+   document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = Math.random() * 100000 | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm('contact_service', 'contact_form', this)
+          .then(function() {
+              console.log('SUCCESS!');
+          }, function(error) {
+              console.log('FAILED...', error);
+          });
+  });
+} */
