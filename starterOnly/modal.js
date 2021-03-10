154c6c060b48253180726ebf4e202502bfd148d7
SNM07 <say.no.more@hotmail.fr>
2021-03-10 16:50:27 +0100

    #4 - WIP v5
    test

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -61,6 +61,53 @@ function validate() {
         });
   }); 
   e.preventDefault(e);*/
+
+  const varsForm = {
+    first: document.getElementById("first"),
+    last: document.getElementById("last"),
+    email: document.getElementById("email"),
+    birthdate: document.getElementById("birthdate"),
+    quantity: document.getElementById("quantity"),
+    NewYork: document.getElementById("location1").checked,
+    SanFrancisco: document.getElementById("location2").checked,
+    Seattle: document.getElementById("location3").checked,
+    Chicago: document.getElementById("location4").checked
+    Boston: document.getElementById("location5").checked,
+    Portland: document.getElementById("location6").checked
+};
+
+emailjs.send('service_fqmxk3h', 'template_zhusj29', varsForm)
+    .then(function() {
+        console.log('SUCCESS!');
+    }, function(error) {
+        console.log('FAILED...', error);
+    });
+
+var textNewYork = 'Non';
+  if (document.getElementById("location1").checked) {
+      textNewYork = 'Oui';
+  }
+var textSanFrancisco = 'Non';
+  if (document.getElementById("location2").checked) {
+      textSanFrancisco = 'Oui';
+  }
+var textSeattle = 'Non';
+  if (document.getElementById("location3").checked) {
+      textSeattle = 'Oui';
+  }
+var textChicago = 'Non';
+  if (document.getElementById("location4").checked) {
+      textChicago = 'Oui';
+  }
+var textBoston = 'Non';
+  if (document.getElementById("location5").checked) {
+      textBoston = 'Oui';
+  }
+var textPortland = 'Non';
+  if (document.getElementById("location6").checked) {
+      textPortland = 'Oui';
+  }
+
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
   //setTimeout(validate(), 8000);
@@ -71,14 +118,14 @@ function validate() {
   return false;
 }
 
-
-/*window.onload = function() {
+/*
+window.onload = function() {
    document.getElementById('contact-form').addEventListener('submit', function(event) {
       event.preventDefault();
       // generate a five digit number for the contact_number variable
       this.contact_number.value = Math.random() * 100000 | 0;
       // these IDs from the previous steps
-      emailjs.sendForm('contact_service', 'contact_form', this)
+      emailjs.sendForm('service_fqmxk3h', 'template_zhusj29', this)
           .then(function() {
               console.log('SUCCESS!');
           }, function(error) {
